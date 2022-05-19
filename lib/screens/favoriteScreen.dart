ce5231946aa17f17719f73675dee0a644e44284e
MuhammedShadi <muhammed.samir.abushadi@gmail.com>
2022-05-20 00:59:28 +0300

    Delete lib directory

--- lib/screens/favoriteScreen.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-class FavoriteScreen extends StatelessWidget {
-  static const screenRout = '/Favorite';
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(child: Text("as"),);
-  }
-}
